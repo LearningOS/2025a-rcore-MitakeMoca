fd6f01a084de0552f1a2095b904e9d9973d10b5d
MitakeMoca <1043530132@qq.com>
2025-09-29 23:56:17 +0800

    feat: finish ch5 tests

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -1,12 +1,14 @@
 //!Implementation of [`TaskManager`]
 use super::TaskControlBlock;
 use crate::sync::UPSafeCell;
-use alloc::collections::VecDeque;
 use alloc::sync::Arc;
+use alloc::vec::Vec;
 use lazy_static::*;
 ///A array of `TaskControlBlock` that is thread-safe
 pub struct TaskManager {
-    ready_queue: VecDeque<Arc<TaskControlBlock>>,
+    /// 为了实现优先级调度，这里最好还是使用优先队列
+    /// 不过这样要改的东西太多了，我们为了简单就使用 Vector
+    ready_queue: Vec<Arc<TaskControlBlock>>,
 }
 
 /// A simple FIFO scheduler.
@@ -14,16 +16,31 @@ impl TaskManager {
     ///Creat an empty TaskManager
     pub fn new() -> Self {
         Self {
-            ready_queue: VecDeque::new(),
+            ready_queue: Vec::new(),
         }
     }
     /// Add process back to ready queue
     pub fn add(&mut self, task: Arc<TaskControlBlock>) {
-        self.ready_queue.push_back(task);
+        self.ready_queue.push(task);
     }
     /// Take a process out of the ready queue
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
-        self.ready_queue.pop_front()
+        if self.ready_queue.len() == 0 {
+            return None;
+        }
+        let mut ret = 0;
+        for i in 0..self.ready_queue.len() {
+            let tem = self.ready_queue[i].inner_exclusive_access();
+            let a = tem.stride;
+            drop(tem);
+            if a < self.ready_queue[ret].inner_exclusive_access().stride {
+                ret = i;
+            }
+        }
+        let tem = self.ready_queue.remove(ret);
+        let priority = tem.inner_exclusive_access().priority;
+        tem.inner_exclusive_access().stride += 10000 / priority;
+        Some(tem)
     }
 }
 
